ca359b4694fedcce5b8495f491bd507c50304627
keshew <artyom.korotckov2017@yandex.ru>
2024-12-09 15:46:02 +0500

    Initial Commit

---
+++ DrawIt/DrawItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawItApp.swift
+//  DrawIt
+//
+//  Created by Артём Коротков on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
